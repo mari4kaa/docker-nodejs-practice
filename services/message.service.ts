a9af4c30efa61d1592915573fbd180ab7513e6fd
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-04-16 18:00:46 +0300

    Change the displayed text
    It was made to test how good cache in Dockerfile works

--- services/message.service.ts
+++ services/message.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MessageService {
   getMessage(): string {
-    return 'Made by Mariia Kryvokhata!';
+    return 'Made by Mariia Kryvokhata!!!';
   }
 }
